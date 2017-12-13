e040ed408e8b300c67e6745c3e020a3e862188c6
Jenkins, Daniel <daniejenkins@paypal.com>
2017-12-13 11:07:42 +0100

    Add asimov-mocha-reporter

---
+++ readme.md
@@ -0,0 +1,17 @@
+### Mocha-asimov-reporter
+
+Reporter to be used with asimov deploy.
+
+#### Setup
+
+1. Add the reporter to package.json
+2. add to mocha command line `--reporter mocha-asimov-reporter`
+
+
+### Output
+* Reporter only supports pass/fail for asimov;
+* Tests that are skipped will be ignored
+
+![](reporter-screenshot.png)
+
+
---
+++ src/asimov-reporter.js
@@ -0,0 +1,31 @@
+/**
+ * Asimov reporter for mocha;
+ */
+var mocha = require('mocha');
+
+function writeResult(message, status) {
+    console.log(`##asimov-deploy[test='${message}' pass='${status}']\n\n`);
+}
+
+function AsimovReporter(runner) {
+    mocha.reporters.Base.call(this, runner);
+    var passes = 0;
+    var failures = 0;
+
+    runner.on('pass', function (test) {
+        passes++;
+        writeResult(test.fullTitle(), 'true');
+    });
+
+    runner.on('fail', function (test, err) {
+        failures++;
+        writeResult(test.fullTitle(), 'false');
+    });
+
+    runner.on('end', function () {
+        console.log('end: %d/%d', passes, passes + failures);
+        process.exit(failures);
+    });
+}
+
+module.exports = AsimovReporter;
